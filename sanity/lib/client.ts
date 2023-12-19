d2255b9d6321d50f4ecd978e79599ab5b7f3e5f1
Ethan <ethanbutformal@gmail.com>
2023-12-19 03:28:53 -0800

    Update environment variables in sanity/env.ts

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,9 +1,10 @@
 import { createClient } from 'next-sanity';
-import { apiVersion, dataset, projectId, useCdn } from '../env';
+import { apiVersion, dataset, projectId, token, useCdn } from '../env';
 
 export const client = createClient({
   apiVersion,
   dataset,
   projectId,
+  token,
   useCdn,
 });
