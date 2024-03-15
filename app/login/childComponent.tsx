9be1ede4bee985de0690d052b42b492d9d097560
Ethan <ethanbutformal@gmail.com>
2024-03-15 05:10:31 -0700

    Removed next-auth logic from login page

--- app/login/childComponent.tsx
+++ app/login/childComponent.tsx
@@ -1,26 +1,34 @@
 'use client';
 
-import { signIn, signOut, useSession } from 'next-auth/react';
+// import { signIn, signOut, useSession } from 'next-auth/react';
 
 export default function Component() {
-    const { data: session, status } = useSession();
-    const userEmail = session?.user?.email;
+    // const { data: session, status } = useSession();
+    // const userEmail = session?.user?.email;
 
-    if (status === 'loading') {
-        return <p>Hang on there...</p>;
+    function signOut() {
+        console.log('idk sign out');
     }
 
-    if (status === 'authenticated') {
-        return (
-            <>
-                <p>Signed in as {userEmail}</p>
-                <div>
-                    <button onClick={() => signOut()}>Sign out</button>
-                </div>
-                <img src='https://cdn.pixabay.com/photo/2017/08/11/19/36/vw-2632486_1280.png' />
-            </>
-        );
+    function signIn() {
+        console.log('idk sign in');
     }
+
+    // if (status === 'loading') {
+    //     return <p>Hang on there...</p>;
+    // }
+    //
+    // if (status === 'authenticated') {
+    //     return (
+    //         <>
+    //             <p>Signed in as ...</p>
+    //             <div>
+    //                 <button onClick={() => signOut()}>Sign out</button>
+    //             </div>
+    //             <img src='https://cdn.pixabay.com/photo/2017/08/11/19/36/vw-2632486_1280.png' />
+    //         </>
+    //     );
+    // }
 
     return (
         <div>
