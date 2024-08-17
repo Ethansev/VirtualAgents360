3960c7b886c0af26fc28c07f4599efc57c7444fe
Ethansev <ethanbutformal@gmail.com>
2024-08-17 00:30:38 -0700

    Updated sanity schema for important info and procedures

--- src/services/sanity/schemas/procedure-info.ts
+++ src/services/sanity/schemas/procedure-info.ts
@@ -1,32 +1,32 @@
-import { defineArrayMember, defineField, defineType } from 'sanity';
+import { defineArrayMember, defineField, defineType } from '@sanity-typed/types';
 
 export const procedureInfo = defineType({
-  name: 'procedure_info',
-  title: 'Important Info & Procedures',
-  type: 'document',
-  fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    }),
-    defineField({
-      name: 'body',
-      title: 'Body',
-      type: 'array',
-      of: [
-        defineArrayMember({
-          type: 'block',
-          marks: {
-            decorators: [
-              { title: 'Strong', value: 'strong' },
-              { title: 'Emphasis', value: 'em' },
-              { title: 'Code', value: 'code' },
-              { title: 'Underline', value: 'underline', icon: () => 'U' },
-            ],
-          },
+    name: 'procedure_info',
+    title: 'Important Info & Procedures',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'title',
+            title: 'Title',
+            type: 'string',
         }),
-      ],
-    }),
-  ],
+        defineField({
+            name: 'body',
+            title: 'Body',
+            type: 'array',
+            of: [
+                defineArrayMember({
+                    type: 'block',
+                    marks: {
+                        decorators: [
+                            { title: 'Strong', value: 'strong' },
+                            { title: 'Emphasis', value: 'em' },
+                            { title: 'Code', value: 'code' },
+                            { title: 'Underline', value: 'underline', icon: () => 'U' },
+                        ] as const,
+                    },
+                }),
+            ],
+        }),
+    ],
 });
